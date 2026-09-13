bdf44a446529b6aaa183d774866061cc6c34b893
MaferCorzo <mariafernandacoca@ufps.edu.co>
2026-09-13 13:23:16 -0500

    feat: Agregar identificador

--- arranque.sh
+++ arranque.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 apt update && apt install -y nginx
-echo "<h1><identificacion></h1><p>Servida desde Terraform por $(hostname)</p>" > /var/www/html/index.html
+echo "<h1><Grupo 6></h1><p>Servida desde Terraform por $(hostname)</p>" > /var/www/html/index.html
