b55abdecee02e797b8ed6ed627b606d362a43c18
MaferCorzo <mariafernandacoca@ufps.edu.co>
2026-09-13 15:04:23 -0500

    fix: correcion eror main

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ terraform {
       version = ">= 6.0"
     }
   }
-
-  backend "gcs" {
-    bucket = "tfstate- maquinavirtual-507221"
+    backend "gcs" {
+    bucket = "tfstate-maquinavirtual-507221"
     prefix = "practica-2"
   }
+
 }
 
 provider "google" {
